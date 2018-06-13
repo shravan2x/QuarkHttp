b6e19b9015e0ccd5705f5c7c869740282d197a3f
Shravan Rajinikanth <mail@shrav.me>
2018-06-12 21:14:32 -0500

    Updated QCollectionUtils removing dependency on System.Web
    - #JustPortingToNetStandardThings.

--- QuarkHttp/QCollectionUtils.cs
+++ QuarkHttp/QCollectionUtils.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
-using System.Text;
-using System.Web;
+using System.Linq;
 
 namespace QuarkHttp
 {
@@ -9,19 +8,9 @@ namespace QuarkHttp
     {
         public static string MakeString(this NameValueCollection paramMap, string assign, string concat, bool urlEncode = false)
         {
-            StringBuilder paramStringBuilder = new StringBuilder();
-
-            for (int index = 0; index < paramMap.Count; index++)
-            {
-                string[] paramValues = paramMap.GetValues(index) ?? new string[1];
-
-                paramStringBuilder.Append(!urlEncode ? $"{paramMap.GetKey(index)}{assign}{paramValues[0]}" : $"{HttpUtility.UrlEncode(paramMap.GetKey(index))}{assign}{HttpUtility.UrlEncode(paramValues[0])}");
-
-                if (index < paramMap.Count - 1)
-                    paramStringBuilder.Append(concat);
-            }
-
-            return paramStringBuilder.ToString();
+            return String.Join(concat, paramMap
+                .Cast<string>()
+                .Select(x => (!urlEncode ? $"{x}{assign}{paramMap[x]}" : $"{Uri.EscapeDataString(x)}{assign}{Uri.EscapeDataString(paramMap[x])}")));
         }
 
         public static NameValueCollection MakeCollection(this string paramString, string assign, string concat, bool urlDecode = false)
@@ -39,7 +28,7 @@ namespace QuarkHttp
                 if (!urlDecode)
                     paramMap[pair.Substring(0, assignLoc)] = pair.Substring(assignLoc + 1);
                 else
-                    paramMap[HttpUtility.UrlDecode(pair.Substring(0, assignLoc))] = HttpUtility.UrlDecode(pair.Substring(assignLoc + 1));
+                    paramMap[Uri.UnescapeDataString(pair.Substring(0, assignLoc))] = Uri.UnescapeDataString(pair.Substring(assignLoc + 1));
             }
 
             return paramMap;
