e3af269204221505b07cdfe4632a4325ce2b0e8e
Shravan Rajinikanth <mail@shrav.me>
2018-06-13 18:25:54 -0500

    Added QuarkHttp.Payloads.Json project

---
+++ QuarkHttp.Payloads.Json/QJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QuarkHttp.Payloads.Json
+{
+    public static class QJsonConverter
+    {
+        private static readonly JsonSerializer DefaultSerializer = JsonSerializer.CreateDefault();
+
+        // TODO: Should be made async after https://github.com/JamesNK/Newtonsoft.Json/issues/66.
+        public static T AsJson<T>(this QPayload payload, JsonSerializer serializer = null)
+        {
+            if (serializer == null)
+                serializer = DefaultSerializer;
+
+            using (StreamReader payloadReader = new StreamReader(payload.Stream))
+            using (JsonTextReader payloadJsonReader = new JsonTextReader(payloadReader))
+                return serializer.Deserialize<T>(payloadJsonReader);
+        }
+    }
+}
---
+++ QuarkHttp.Payloads.Json/QJsonPayload.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QuarkHttp.Payloads.Json
+{
+    // TODO: Find out how to dispose of things correctly.
+    /*public class QJsonPayload : QPayload
+    {
+        private static readonly JsonSerializer DefaultSerializer = JsonSerializer.CreateDefault();
+
+        public QJsonPayload(object obj, JsonSerializer serializer = null, Encoding encoding = null) : base(ToStream(obj, serializer, encoding)) { }
+
+        private static Stream ToStream(object obj, JsonSerializer serializer = null, Encoding encoding = null)
+        {
+            if (serializer == null)
+                serializer = DefaultSerializer;
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            MemoryStream outputStream = new MemoryStream();
+            serializer.Serialize(new StreamWriter(outputStream, encoding), obj);
+
+            return outputStream;
+        }
+    }*/
+}
