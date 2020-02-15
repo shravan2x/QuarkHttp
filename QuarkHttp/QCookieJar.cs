29ef47d3379dc599937db5cddce7e2849ea437ae
Shravan Rajinikanth <mail@shrav.me>
2020-02-15 14:57:31 -0800

    Fixed IOBE on empty input to QCookieJar.MapCookies

--- QuarkHttp/QCookieJar.cs
+++ QuarkHttp/QCookieJar.cs
@@ -9,8 +9,10 @@ namespace QuarkHttp
         public static CookieCollection MapCookies(string cookieString)
         {
             CookieCollection cookies = new CookieCollection();
-            string[] cookieArray = cookieString.Split(';');
+            if (cookieString == "")
+                return cookies;
 
+            string[] cookieArray = cookieString.Split(';');
             foreach (string curCookie in cookieArray)
             {
                 string[] cookieParts = curCookie.Trim().Split('=');
@@ -32,7 +34,7 @@ namespace QuarkHttp
                 if (cookies[index].Expired)
                     continue;
 
-                string curCookieString = String.Format("{0}={1}", cookies[index].Name, cookies[index].Value);
+                string curCookieString = $"{cookies[index].Name}={cookies[index].Value}";
                 cookieStringBuilder.Append(curCookieString);
 
                 if (index < cookies.Count - 1)
