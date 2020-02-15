315dcc9068088a5ec3971debea697335cb293353
Shravan Rajinikanth <mail@shrav.me>
2020-02-15 15:26:29 -0800

    Refactored QHttpRequestFacts test

--- Tests/QHttpRequestFacts.cs
+++ Tests/QHttpRequestFacts.cs
@@ -3,20 +3,20 @@ using Xunit;
 
 namespace Tests
 {
-    public class QHttpRequestFacts
+    public static class QHttpRequestFacts
     {
         [Fact]
-        public void PassingTest()
+        public static void TestQueryParams()
         {
-            QHttpRequest r = new QHttpRequest("https://google.com");
-            r.QueryParams["session"] = "data=";
-            Assert.Equal("https://google.com/?session=data%3D", r.Url);
-            r.QueryParams["r"] = "3";
-            Assert.Equal("https://google.com/?session=data%3D&r=3", r.Url);
-            r.QueryParams["r"] = "4";
-            Assert.Equal("https://google.com/?session=data%3D&r=4", r.Url);
-            r.QueryParams["r"] = "5";
-            Assert.Equal("https://google.com/?session=data%3D&r=5", r.Url);
+            QHttpRequest req = new QHttpRequest("https://google.com");
+            req.QueryParams["session"] = "data=";
+            Assert.Equal("https://google.com/?session=data%3D", req.Url);
+            req.QueryParams["r"] = "3";
+            Assert.Equal("https://google.com/?session=data%3D&r=3", req.Url);
+            req.QueryParams["r"] = "4";
+            Assert.Equal("https://google.com/?session=data%3D&r=4", req.Url);
+            req.QueryParams["r"] = "5";
+            Assert.Equal("https://google.com/?session=data%3D&r=5", req.Url);
         }
     }
 }
